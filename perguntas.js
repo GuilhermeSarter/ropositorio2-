34a3caf25f59d291eb4b43321d3024f8c5428b69
GuilhermeSarter <guilherme.sarter@escola.pr.gov.br>
2025-07-30 08:05:30 -0300

    sfd

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Biologia',
+    'O que é uma Enzima?',
+    'é uma proteina responsavel por catalizar as atividades metabolicas que ocorrem no corpo, que naturalmente demora'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é DNA?',
+    'é um acido nucleico responsavel por guardar informaçoes geneticas de quase todos os seres vivos (podemos comparar com um livro de receitas de como produzir determinado ser vivo)'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é Homoestase?',
+    'é o processo pelo qual os organismos mantêm um ambiente interno estável e constante, apesar das variações externas'
+)
