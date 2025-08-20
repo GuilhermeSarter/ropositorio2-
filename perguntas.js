9c6cc5a69d7f21e964d538e3ea0aab1afa01ff9e
GuilhermeSarter <guilherme.sarter@escola.pr.gov.br>
2025-08-20 08:08:49 -0300

    das

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,21 @@ criaCartao(
     'O que é Homoestase?',
     'é o processo pelo qual os organismos mantêm um ambiente interno estável e constante, apesar das variações externas'
 )
+
+criaCartao(
+    'Biologia',
+    'Qual a função dos ribossomos?',
+    'São responsáveis pela síntese de proteínas a partir da informação genética contida no RNA mensageiro.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que são enzimas?',
+    'São proteínas que atuam como catalisadores biológicos, acelerando as reações químicas nas células.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a diferença entre célula procariótica e eucariótica?',
+    'A célula procariótica não possui núcleo organizado nem organelas membranosas, enquanto a eucariótica possui núcleo definido e organelas especializadas.'
+)
